b708b5193433fd9bb0f5e9a276745a17c7f1a4f3
zairjmg <jair.jmgu@gmail.com>
2023-02-13 20:37:13 -0500

    dia 2

---
+++ Dia 2/js/app.js
@@ -0,0 +1,53 @@
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circle = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+progress.style.width = '0%'
+
+next.addEventListener('click', () => {
+    currentActive++
+    console.log(currentActive);
+    if (currentActive > circle.length) {
+        currentActive = circle.length
+    }
+    
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+    console.log(currentActive);
+    if (currentActive < 1) {
+        currentActive = 1
+    }
+    
+    update()
+})
+
+function update() {
+    circle.forEach((circle, i) => {
+        if (i < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    });
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circle.length - 1) * 100 + '%'
+
+    console.log(progress.style.width);
+
+    if (currentActive === 1 ) {
+        prev.disabled = true
+    } else if (currentActive === circle.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
