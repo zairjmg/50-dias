f6228be798f71a9102b80bee98eb82398ada9099
zairjmg <jair.jmgu@gmail.com>
2023-02-16 16:24:03 -0500

    dia 9

--- Dia 9/js/app.js
+++ Dia 9/js/app.js
@@ -1,1 +1,24 @@
- javascript 
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const buttons = document.getElementById('buttons')
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerHTML = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+    buttons.appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0
+    })
+}
