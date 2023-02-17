1ea24491fe87429ebf23dbe094549d363e20c98a
zairjmg <jair.jmgu@gmail.com>
2023-02-16 22:13:17 -0500

    Dia 12

--- Dia 12/js/app.js
+++ Dia 12/js/app.js
@@ -1,1 +1,8 @@
- javascript 
+const toggles = document.querySelectorAll('.faq-toggle')
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        console.log(toggle);
+        toggle.parentNode.classList.toggle('active')
+    })
+})
