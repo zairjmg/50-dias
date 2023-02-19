3a20d8ddb8d58679bffb86bfbc8d1360be923d88
zairjmg <jair.jmgu@gmail.com>
2023-02-18 21:03:28 -0500

    Dia 15

--- Dia 15/js/app.js
+++ Dia 15/js/app.js
@@ -1,1 +1,17 @@
- javascript 
+var counters = document.querySelectorAll('.counter');
+counters.forEach(function (counter) {
+    counter.innerText = '0';
+    var updateCounter = function () {
+        var target = +counter.getAttribute('data-target');
+        var c = +counter.innerText;
+        var increment = target / 200;
+        if (c < target) {
+            counter.innerText = "".concat(Math.ceil(c + increment));
+            setTimeout(updateCounter, 1);
+        }
+        else {
+            counter.innerText = "".concat(target);
+        }
+    };
+    updateCounter();
+});
